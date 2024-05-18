6ca15e2a6ae2e576cc1a5f943a53276957c14930
Martina Ristovska <mristovska861@gmail.com>
2024-05-18 17:07:52 +0200

    done ex-2

---
+++ index.js
@@ -0,0 +1,14 @@
+//2. Checking Day of the Week
+//- Monday - Start of the work week!
+//- Wednesday - Midweek!
+//- Friday - Almost weekend!
+//- Other day - Just another day.
+let dayOfWeek='Thursday'
+
+if(dayOfWeek==='Monday'){
+    console.log('Start of the work week!')
+}else if (dayOfWeek==='Wednesday'){
+    console.log('Wednesday')
+}else{
+    console.log('Just another day')
+}
